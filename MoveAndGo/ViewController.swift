9c1578b03973a752e600202e80d7adc6f094eb53
MAC <>
2019-12-17 20:33:47 +0100

    Initial

---
+++ MoveAndGo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MoveAndGo
+//
+//  Created by MAC on 17/12/2019.
+//  Copyright © 2019 EdJordan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
